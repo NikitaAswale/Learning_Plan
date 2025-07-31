e149179b3d8e47288839bd65a90e6e16e2a65494
NikitAswale <aswalenikita26@gmail.com>
2025-07-31 19:25:10 +0530

    Updated_UI

--- app/src/main/java/com/example/learning_plan/Composable.kt
+++ app/src/main/java/com/example/learning_plan/Composable.kt
@@ -1,7 +1,9 @@
 package com.example.learning_plan
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,8 +12,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
@@ -21,9 +27,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.InspectableModifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -61,10 +71,10 @@ fun Learning_Plate() {
                         modifier = Modifier
                             .height(48.dp)
                             .width(48.dp)
-                            .clip(RoundedCornerShape(6.dp))
+                            .clip(CircleShape)
                     )
                 Column {
-                    Row (modifier = Modifier.padding(4.dp),
+                    Row (modifier = Modifier.padding(6.dp),
                         verticalAlignment = Alignment.CenterVertically){
                         Text(
                             text = "Debbie K",
@@ -72,7 +82,7 @@ fun Learning_Plate() {
                             fontWeight = FontWeight.Bold
                         )
                     }
-                    Row (modifier = Modifier.fillMaxWidth().padding(start=4.dp),
+                    Row (modifier = Modifier.fillMaxWidth().padding(start=6.dp),
                         verticalAlignment = Alignment.CenterVertically){
                         Text(
                             text = "Product Designer",
@@ -167,6 +177,45 @@ fun Learning_Plate() {
                 }
             }
         }
+        Spacer(modifier = Modifier.height(4.dp))
 
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFFADD8E6)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            Row(modifier = Modifier.fillMaxWidth().padding(top = 16.dp,start =16.dp, end = 16.dp)) {
+                Text(text = "1311 points away from the top",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold)
+            }
+            Row(modifier = Modifier.fillMaxWidth().padding(start =16.dp, end = 16.dp,bottom = 16.dp)){
+                Text(text = "You can earn points by completing content and by earning badges",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal)
+            }
+        }
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            Button(onClick = {},
+                modifier = Modifier.padding(8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFA500)),
+                shape = RoundedCornerShape(6.dp)
+            ) {
+                Text(text = " Goto My Learning Plan",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    textAlign = TextAlign.Center)
+            }
+        }
     }
 }
